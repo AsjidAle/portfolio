38d9c254f3a41a3c7b42ba0c650e4e184c75d5de
Asjid Ali <asjidale@gmail.com>
2025-02-11 09:53:42 +0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
-  basePath: "/portfolio", // ✅ Fix GitHub Pages path issue
-  assetPrefix: "/portfolio/", // ✅ Ensures assets load correctly
+  output: process.env.NODE_ENV !== "development" ? "export" : undefined,
+  basePath: process.env.NODE_ENV !== "development" ? "/portfolio" : "",
+  assetPrefix: process.env.NODE_ENV !== "development" ? "/portfolio/" : "",
 };
 
 export default nextConfig;
